27496acd5890541ec8a2c3e13a0cbbbb05a8fde8
Mago49 <pedrimafc@gmail.com>
2026-09-11 21:33:15 -0300

    Implement SAFE_MODE functionality in main.js
    Add SAFE_MODE check to display safe mode banner.

--- app-novo/js/main.js
+++ app-novo/js/main.js
@@ -1,8 +1,12 @@
 import { startBackgroundAnimation } from './ui-background.js';
 import { initAuth } from './auth-guard.js';
 import { initRouter } from './router.js';
+import { SAFE_MODE } from './dev-flags.js';
 
 startBackgroundAnimation();
+ if (SAFE_MODE) {
+  document.getElementById('safeModeBanner').classList.remove('app-hidden');
+}
 
 initAuth({
   onLogin: () => initRouter(),
